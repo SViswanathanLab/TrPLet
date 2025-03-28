ff117abbc48cdc6faa7701320d8f0e7e29a59f71
Yantong Cui <143852554+yanc0828@users.noreply.github.com>
2025-03-28 15:53:01 -0400

    Update step2_merge.py

--- snakemake_TrPLet/scripts/step2_merge.py
+++ snakemake_TrPLet/scripts/step2_merge.py
@@ -11,15 +11,12 @@ def round_numeric_columns(df, decimals):
     return df
 
 df = pd.read_csv(snakemake.input[0], compression='gzip', header=0, sep='\t', quotechar='"')
-#df = pd.read_csv("/mnt/storage/home/yc638/0327_drost/data/merged_TCGA_geneLevel_CountMatrix.tsv.gz", compression='gzip', header=0, sep='\t', quotechar='"')
 
-# df_ids = df['Unnamed: 0'].tolist()
 df['revised_IDs'] = df['gene_name']
 del df['gene_name'] #ID format: ARF5
 #ID format: ENST00000000233.9|ENSG00000004059.10|OTTHUMG00000023246.6|OTTHUMT00000059567.2|ARF5-001|ARF5|1103|protein_coding|
 
 df2 = pd.read_csv(snakemake.input[1], sep="\t")
-df2 = pd.read_csv("/mnt/storage/home/yc638/0327_drost/data/gene_count_matrix.txt", sep="\t")
 df2_ids = df2['gene_id'].tolist()
 #ID format: 'ENSG00000000005.6_TNMD'
 
@@ -28,14 +25,6 @@ df2_ids_proc = []
 for a in df2_ids:
     df2_ids_proc.append(a.split(".")[0])
     df2_genes.append(a.split("_")[-1])
-
-# df_ids_proc = []
-# for a in df_ids:
-#     g_id = a.split("|")[1]
-#     df_ids_proc.append(g_id.split(".")[0])
-
-# df['revised_IDs'] = df_ids_proc
-# del df['Unnamed: 0']
 
 df2.insert(0,'genes',df2_genes)
 df2.insert(0,'revised_IDs',df2_ids_proc)
