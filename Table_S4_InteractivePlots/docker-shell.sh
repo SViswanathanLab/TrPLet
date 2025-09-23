08efcefbfe3e6bd8ef8d20cfdb3944c7ceea18a6
Yantong Cui <143852554+yanc0828@users.noreply.github.com>
2025-09-23 11:19:09 -0400

    Update docker-shell.sh

--- Table_S4_InteractivePlots/docker-shell.sh
+++ Table_S4_InteractivePlots/docker-shell.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Build image
-docker build -t trplet-app .
+# docker build -t trplet-app .
+
+docker build --platform linux/amd64 -t trplet-app .
 
 # Run container
 docker run --rm -ti -p 3838:3838 trplet-app
